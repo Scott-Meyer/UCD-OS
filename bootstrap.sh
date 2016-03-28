bd98e8e73dc1a86602af7ce86867fb35163c7fc5
Sean Meyer <Sean Meyer>
2016-03-28 12:55:05 -0600

    Added "tree" program to the vagrant machine

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,6 @@ apt-get upgrade -y
 apt-get install -y cmake
 apt-get install -y git
 apt-get install -y qemu-system
+apt-get install -y tree
 sudo echo 'export PATH=/home/git-repos/UCD-OS/src/utils:$PATH' >> /home/vagrant/.profile
 source /home/vagrant/.profile
